1d104ca4b8256d305c09e3732c75b3d82354fe01
deanlauvina7 <deanlauvina7@gmail.com>
2020-12-01 06:40:15 -0600

    Getting the dates

---
+++ src/api.js
@@ -0,0 +1,26 @@
+// Base URL
+const base_url = 'https://api.rawg.io/api';
+
+// Getting the date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+
+// Getting the date
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+// Popular Games
